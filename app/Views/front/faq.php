0ac3599dfe69905b3ba5b876806f9b26b9db2540
Melisa Lezcano Airaldi <melisalezcanoairaldi@gmail.com>
2024-04-26 17:21:38 -0300

    Removed cache directory

--- app/Views/front/faq.php
+++ app/Views/front/faq.php
@@ -125,13 +125,13 @@
         
 
         <h2 class="faq-pregunta">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
             <strong>¿Son adecuadas sus urnas para uso en exteriores?</strong>
           </button>
         </h2>
 
 
-        <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+        <div id="flush-collapseSix" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
           <div class="faq-respuesta">Nuestras urnas están diseñadas principalmente para uso en interiores. Si bien algunas pueden ser adecuadas para exhibición en exteriores, recomendamos consultar las descripciones de los productos o contactarnos para recomendaciones específicas.</div>
         </div>
 
@@ -145,13 +145,13 @@
 
 
         <h2 class="faq-pregunta">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
           <strong>¿Las velas conmemorativas vienen con un portavelas?</strong>
           </button>
         </h2>
 
 
-        <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+        <div id="flush-collapseSeven" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
           <div class="faq-respuesta">Algunas de nuestras velas conmemorativas pueden venir con un portavelas, mientras que otras pueden requerir una compra por separado. Consulta las descripciones individuales de los productos para obtener detalles.</div>
         </div>
 
@@ -163,13 +163,13 @@
 
 
         <h2 class="faq-pregunta">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEight" aria-expanded="false" aria-controls="flush-collapseEight">
           <strong>¿Puedo personalizar el tamaño de la joyería para cenizas?</strong>
           </button>
         </h2>
 
 
-        <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+        <div id="flush-collapseEight" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
           <div class="faq-respuesta">Nuestra joyería para cenizas generalmente viene en tamaños estándar. Sin embargo, algunos artículos pueden ofrecer opciones de personalización. Consulta las descripciones de los productos o contáctanos para obtener más información.</div>
         </div>
 
@@ -181,13 +181,13 @@
 
 
         <h2 class="faq-pregunta">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseNine" aria-expanded="false" aria-controls="flush-collapseNine">
           <strong>¿Ofrecen descuentos para pedidos al por mayor?</strong>
           </button>
         </h2>
 
 
-        <div id="flush-collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+        <div id="flush-collapseNine" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
           <div class="faq-respuesta">Podemos ofrecer descuentos para pedidos al por mayor caso por caso. Por favor, contacta a nuestro equipo de servicio al cliente para preguntar sobre opciones de precios al por mayor.</div>
         </div>
 
@@ -198,13 +198,13 @@
 
 
         <h2 class="faq-pregunta">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTen" aria-expanded="false" aria-controls="flush-collapseTen">
           <strong>¿Qué materiales se utilizan en sus productos?</strong>
           </button>
         </h2>
 
 
-        <div id="flush-collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+        <div id="flush-collapseTen" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
           <div class="faq-respuesta">Nuestros productos están fabricados con una variedad de materiales, incluyendo madera, metal, cerámica y vidrio. Consulta las descripciones individuales de los productos para obtener detalles específicos de los materiales.</div>
         </div>
 
